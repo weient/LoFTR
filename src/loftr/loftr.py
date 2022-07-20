7f70b90c0b1c3b232bc3421d882807f13a209bbc
weient <weien@gapp.nthu.edu.tw>
2022-07-20 20:52:39 +0800

    add feature to

--- src/loftr/loftr.py
+++ src/loftr/loftr.py
@@ -43,44 +43,28 @@ class LoFTR(nn.Module):
             'bs': data['image0'].size(0),
             'hw0_i': data['image0'].shape[2:]
         })
-        #, 'hw1_i': data['image1'].shape[2:]
-
-        #if data['hw0_i'] == data['hw1_i']:  # faster & better BN convergence
-        #    feats_c, feats_f = self.backbone(torch.cat([data['image0'], data['image1']], dim=0))
-        #    (feat_c0, feat_c1), (feat_f0, feat_f1) = feats_c.split(data['bs']), feats_f.split(data['bs'])
-        #else:  # handle different input shapes
-        (feat_c0, feat_f0) = self.backbone(data['image0'])#, self.backbone(data['image1'])
-        #, (feat_c1, feat_f1)
+        
+        (feat_c0, feat_f0) = self.backbone(data['image0'])
+        
         data.update({
-            'hw0_c': feat_c0.shape[2:],#, 'hw1_c': feat_c1.shape[2:],
-            'hw0_f': feat_f0.shape[2:]#, 'hw1_f': feat_f1.shape[2:]
+            'hw0_c': feat_c0.shape[2:],
+            'hw0_f': feat_f0.shape[2:]
         })
 
         # 2. coarse-level loftr module
         # add featmap with positional encoding, then flatten it to sequence [N, HW, C]
         feat_c0 = rearrange(self.pos_encoding(feat_c0), 'n c h w -> n (h w) c')
-        #feat_c1 = rearrange(self.pos_encoding(feat_c1), 'n c h w -> n (h w) c')
-
-        mask_c0 = mask_c1 = None  # mask is useful in training
+        mask_c0 = None  # mask is useful in training
         if 'mask0' in data:
             mask_c0 = data['mask0'].flatten(-2)
-        # mask_c1 = data['mask1'].flatten(-2)
-        # feat_c0, feat_c1 = self.loftr_coarse(feat_c0, feat_c1, mask_c0, mask_c1)
         feat_c0 = self.loftr_coarse(feat_c0, mask_c0)
-        # 3. match coarse-level
-        #self.coarse_matching(feat_c0, feat_c1, data, mask_c0=mask_c0, mask_c1=mask_c1)
-
-        # 4. fine-level refinement
-        #feat_f0_unfold = self.fine_preprocess(feat_f0, feat_c0, data)
-        #if feat_f0_unfold.size(0) != 0:  # at least one coarse level predicted
-        #    feat_f0_unfold = self.loftr_fine(feat_f0_unfold)
-
-        # 5. match fine-level
-        #self.fine_matching(feat_f0_unfold, feat_f1_unfold, data)
+        
+        # 3. generate feature
+        data.update(
+            {'img_feature': feat_c0}
+        )
         self.feat_course = feat_c0
-        #self.feat_fine = feat_f0_unfold
         print("feat_c0: ", feat_c0)
-        #print("feat_f0_unfold: ", feat_f0_unfold)
 
     def load_state_dict(self, state_dict, *args, **kwargs):
         for k in list(state_dict.keys()):
